09567a81b3a18b3cd296640141b1df5b602464f5
Sian Caleb Domasig <49774633+Acid-Free@users.noreply.github.com>
2023-01-30 09:46:33 +0800

    Add functional two operand logic

--- logic.js
+++ logic.js
@@ -1,3 +1,11 @@
+const buttons = document.querySelectorAll("button");
+const output = document.querySelector(".output");
+
+let leftOperand = "";
+let rightOperand = "";
+let currentOperator = "";
+let displayValue = leftOperand;
+
 const add = (num1, num2) => {
   return num1 + num2;
 };
@@ -15,6 +23,9 @@ const divide = (num1, num2) => {
 };
 
 const operate = (operator, num1, num2) => {
+  num1 = parseFloat(num1);
+  num2 = parseFloat(num2);
+
   switch (operator) {
     case "+":
       return add(num1, num2);
@@ -28,3 +39,76 @@ const operate = (operator, num1, num2) => {
       console.error("Invalid operation");
   }
 };
+
+const updateDisplay = () => {
+  displayValue = `${
+    leftOperand ? leftOperand : 0
+  } ${currentOperator} ${rightOperand}`;
+
+  output.innerText = displayValue;
+};
+
+// checks a string if it represents a floating number without fractional part
+const isWholeFloat = (input) => {
+  const containsDot = [...input].includes(".");
+  // if false, should mean that there is fractional part
+  const noFractional = parseInt(input) === parseFloat(input);
+  // if only noFractional is returned, this function would also return true for integers
+  return containsDot && noFractional;
+};
+
+const addButtonEvents = () => {
+  [...buttons].forEach((button) => {
+    button.addEventListener("click", (e) => {
+      switch (e.target.className) {
+        case "equals":
+          leftOperand = operate(currentOperator, leftOperand, rightOperand);
+          rightOperand = "";
+          currentOperator = "";
+          break;
+        case "delete":
+          break;
+        case "reset":
+          break;
+        case "percentage":
+          break;
+        case "dot":
+          if (currentOperator) {
+            if (![...rightOperand].includes(".")) rightOperand += ".";
+          } else {
+            if (![...leftOperand].includes(".")) leftOperand += ".";
+          }
+          break;
+        case "add":
+          currentOperator = "+";
+          break;
+        case "subtract":
+          currentOperator = "-";
+          break;
+        case "multiply":
+          currentOperator = "x";
+          break;
+        case "divide":
+          currentOperator = "/";
+          break;
+        default:
+          if (currentOperator) {
+            rightOperand += e.target.innerText;
+            const rightFloat = isWholeFloat(rightOperand);
+            // converted back to string for dot evaluation
+            rightOperand = parseFloat(rightOperand).toString();
+            if (rightFloat) rightOperand += ".";
+          } else {
+            leftOperand += e.target.innerText;
+            const leftFloat = isWholeFloat(leftOperand);
+            leftOperand = parseFloat(leftOperand).toString();
+            if (leftFloat) leftOperand += ".";
+          }
+      }
+      updateDisplay();
+    });
+  });
+};
+
+addButtonEvents();
+updateDisplay();
